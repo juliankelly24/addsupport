7e30431b039102700b8a03bcf2d2dad96bbfc6b4
Julian Kelly <julian.kelly@aaf.edu.au>
2017-05-16 15:07:35 +1000

    Add files via upload

--- support.sh
+++ support.sh
@@ -12,30 +12,34 @@
   usermod -aG wheel terry
   usermod -aG wheel dalia
 
-then
+#create .ssh folder
+sudo mkdir /home/julian/.ssh
+sudo mkdir /home/melroy/.ssh
+sudo mkdir /home/terry/.ssh
+sudo mkdir /home/dalia/.ssh
+
 #copy pre created folder/file with public key to respective user
-cp /addsupport/julian/authorized_keys /home/julian/.ssh/authorized_keys
-cp /addsupport/melroy/authorized_keys /home/melroy/.ssh/authorized_keys
-cp /addsupport/terry/authorized_keys /home/terry/.ssh/authorized_keys
-cp /addsupport/dalia/authorized_keys /home/dalia/.ssh/authorized_keys
+sudo cp /addsupport/julian/authorized_keys /home/julian/.ssh/authorized_keys
+sudo cp /addsupport/melroy/authorized_keys /home/melroy/.ssh/authorized_keys
+sudo cp /addsupport/terry/authorized_keys /home/terry/.ssh/authorized_keys
+sudo cp /addsupport/dalia/authorized_keys /home/dalia/.ssh/authorized_keys
 
-then
 #Change file / folder permissions
-chmod 700 /home/julian
-chmod 655 /home/julian/.ssh
-chmod 655 /home/julian/.ssh/*
+sudo chmod 700 /home/julian
+sudo chmod 655 /home/julian/.ssh
+sudo chmod 655 /home/julian/.ssh/*
 
-chmod 700 /home/melroy
-chmod 655 /home/melroy/.ssh
-chmod 655 /home/melroy/.ssh/*
+sudo chmod 700 /home/melroy
+sudo chmod 655 /home/melroy/.ssh
+sudo chmod 655 /home/melroy/.ssh/*
 
-chmod 700 /home/terry
-chmod 655 /home/terry/.ssh
-chmod 655 /home/terry/.ssh/*
+sudo chmod 700 /home/terry
+sudo chmod 655 /home/terry/.ssh
+sudo chmod 655 /home/terry/.ssh/*
 
-chmod 700 /home/dalia
-chmod 655 /home/dalia/.ssh
-chmod 655 /home/dalia/.ssh/*
-done
+sudo chmod 700 /home/dalia
+sudo chmod 655 /home/dalia/.ssh
+sudo chmod 655 /home/dalia/.ssh/*
+
 #uncomment line 99 %wheel	ALL=(ALL)	NOPASSWD: ALL
 sed -i '/#%wheel/s/^#//g' /etc/sudoers
